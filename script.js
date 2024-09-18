4c1cc3ab885db2a69e0d9645ca8502ed44cee0f3
ranitchakraborty1 <162864925+ranitchakraborty1@users.noreply.github.com>
2024-09-18 10:20:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function insert_Row() {
     //Write your code here
+	
+	let table = document.getElementById('sampleTable');
+	
+	let newRow = table.insertRow();
+	
+	let leftCell = newRow.insertCell(0);
+	leftCell.innerHTML ="New Cell1";
+	let rightCell = newRow.insertCell(1);
+	rightCell.innerHTML ="New Cell2";
+	
+	
   
   
 }
